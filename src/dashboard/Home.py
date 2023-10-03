a87e991f9823faa9bb3c6db07362831777396f7a
Rayki-Fang <e0511025@u.nus.edu>
2023-10-03 23:48:43 +0800

    New Login Method
    -add new login method
    -now with aws server database
    -only login users can access the pages
    -password reset + email authe features

--- src/dashboard/Home.py
+++ src/dashboard/Home.py
@@ -3,7 +3,18 @@ from src.dashboard.data.data_handling import load_data
 
 st.set_page_config(layout="wide")
 
-st.write("Hello World")
+st.write("Welcome to the dashboard")
 
-metrics_data = load_data().reset_index(drop=True)
-st.write(metrics_data)
+if (
+    st.session_state["authenticated"]
+    and "Admin" in st.session_state["user_cognito_groups"]
+):
+    metrics_data = load_data().reset_index(drop=True)
+    st.write(metrics_data)
+
+else:
+    if st.session_state["authenticated"]:
+        st.write("You do not have access. Please contact the administrator. But here is a sample for what you can see as GUEST :)")
+    else:
+        st.write("Please login!")
+
