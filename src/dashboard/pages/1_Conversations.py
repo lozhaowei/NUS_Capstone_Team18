54dd3006e181bc11cda69ed680c2f0c688fc5242
lozhaowei <lozhaowei@gmail.com>
2023-10-18 10:20:49 +0800

    added selection to show dashbaord elements

--- src/dashboard/pages/1_Conversations.py
+++ src/dashboard/pages/1_Conversations.py
@@ -3,7 +3,7 @@ import streamlit as st
 from src.dashboard.data.data_handling import filter_data
 from src.dashboard.data.database import get_dashboard_data
 from src.dashboard.components import summary_metrics_component, historical_retraining_data_visualisation_component, \
-    user_feedback_component, model_rating_component, generate_pdf_component
+    real_time_data_visualisation_component, user_feedback_component, model_rating_component, generate_pdf_component
 
 st.set_page_config(layout="wide")
 if (
@@ -17,20 +17,29 @@ if (
     col1.title('Conversations')
     models = col2.multiselect('Model', options=model_list, default=model_list[0])
     filtered_data = filter_data(data, models)
+    elements = st.multiselect("Choose the elements that you want to show",
+                            options=["Summary Metrics", "Real Time Data", "Historical Chart", "User Feedback"])
 
     st.divider()
 
-    summary_metrics_component(filtered_data, models)
+    if "Summary Metrics" in elements:
+        summary_metrics_component(filtered_data, models)
+        st.divider()
 
-    st.divider()
+    if "Real Time Data" in elements:
+        real_time_data_visualisation_component()
+        st.divider()
 
-    historical_chart = historical_retraining_data_visualisation_component(filtered_data, models)
+    if "Historical Chart" in elements:
+        historical_chart = historical_retraining_data_visualisation_component(filtered_data, models)
+        st.divider()
 
     # user feedback
-    model_rating_component('video')
-    user_feedback_component('video', model_list)
+    if "User Feedback" in elements:
+        model_rating_component('video')
+        user_feedback_component('video', model_list)
 
-    generate_pdf_component(filtered_data, models, historical_chart)
+    # generate_pdf_component(filtered_data, models, historical_chart)
 
 else:
     if st.session_state["authenticated"]:
