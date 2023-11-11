6a7239fbdc29a6b6d0470e83e910a34002734342
Rayki-Fang <e0511025@u.nus.edu>
2023-11-12 01:06:20 +0800

    Added more roles, changes the way the data updates to aws

--- src/dashboard/Home.py
+++ src/dashboard/Home.py
@@ -1,24 +1,30 @@
 import streamlit as st
 
+st.set_page_config(layout="wide")
+
 from src.dashboard.data.spark_pipeline import SparkPipeline
 from user_authen.authenticate_components import setupemptyprofile, get_role, check_login_status, \
-    generate_captcha, get_manager, login_with_remember_me, cookie_manager
-
-st.set_page_config(layout="wide")
+    login_with_remember_me, cookie_manager
 
 st.write("Welcome to the dashboard")
 
+# Instantiate an empty session state
 st.header(":violet[Login Page]")
 if "username" not in st.session_state or "role" not in st.session_state:
     setupemptyprofile()
 
+# Check the login status to display the "Log In"/"Log Out" buttons
 with st.sidebar:
     st.text("")
     check_login_status()
 
-# st.write(cookie_manager.get_all())
+# Obtain the data from cookie
 cookie_manager.get_all()
 
+# Check if the user has logged in, if yes, then display that he/she has logged in
+# If not, check the cookie to see if there was user account remembered
+# If yes, then ask if the user wants to login through that account
+# If there is no account remembered or if the user wants to login using another account, he will de directed to the login page
 if st.session_state.username is not None:
     st.write("You have already logged in. Click the side bar to sign out. Thank you!")
 else:
